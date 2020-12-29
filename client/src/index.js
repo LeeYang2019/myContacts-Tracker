a33ecba78a41ffa0952304449c662e0664540f00
leeyang2019 <nyang@madisoncollege.edu>
2020-12-29 15:43:49 -0500

    implemented intl-tel-input

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import IntlTelInput from 'react-intl-tel-input';
+// import 'react-intl-tel-input/dist/main.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
