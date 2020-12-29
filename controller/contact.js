3c07bf02501e1fabbb712afd656aabd651c14d0c
leeyang2019 <nyang@madisoncollege.edu>
2020-12-28 23:52:06 -0500

    upgrade yarn

--- controller/contact.js
+++ controller/contact.js
@@ -1,1 +1,13 @@
-export const getContacts = async (req, res) => {};
+const Contact = require('../modals/Contact');
+
+export const getContacts = async (req, res) => {
+	try {
+		const contacts = await Contact.find({ user: req.user.id }).sort({
+			date: -1,
+		});
+		res.status(200).json(contacts);
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send('Server Error');
+	}
+};
