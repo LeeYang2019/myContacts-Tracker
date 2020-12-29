457b2a7d3e827f4cbe7e3ddcf2f4f6832b7d036c
leeyang2019 <nyang@madisoncollege.edu>
2020-12-28 22:35:24 -0500

    updated UI

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -8,91 +8,87 @@ import Typography from '@material-ui/core/Typography';
 import ButtonBase from '@material-ui/core/ButtonBase';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    marginBottom: 25,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    margin: 'auto',
-    maxWidth: 500,
-  },
-  image: {
-    width: 128,
-    height: 128,
-  },
-  img: {
-    borderRadius: '50%',
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
-  },
+	root: {
+		flexGrow: 1,
+		marginBottom: 25,
+	},
+	paper: {
+		padding: theme.spacing(2),
+		margin: 'auto',
+		maxWidth: 500,
+	},
+	image: {
+		width: 128,
+		height: 128,
+	},
+	img: {
+		borderRadius: '50%',
+		margin: 'auto',
+		display: 'block',
+		maxWidth: '100%',
+		maxHeight: '100%',
+	},
 }));
 
 const ContactItem = ({ contact }) => {
-  const contactContext = useContext(ContactContext);
-  const { deleteContact, setCurrent, clearCurrent } = contactContext;
+	const contactContext = useContext(ContactContext);
+	const { deleteContact, setCurrent, clearCurrent } = contactContext;
 
-  const { _id, name, email, avatar, phone, type } = contact;
+	const { _id, name, email, avatar, phone, type } = contact;
 
-  const onDelete = () => {
-    console.log(_id);
-    deleteContact(_id);
-    clearCurrent();
-  };
+	const onDelete = () => {
+		deleteContact(_id);
+		clearCurrent();
+	};
 
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <Grid container spacing={2}>
-          <Grid item>
-            <ButtonBase className={classes.image}>
-              <img className={classes.img} alt="complex" src={avatar} />
-            </ButtonBase>
-          </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <Typography gutterBottom variant="subtitle1">
-                  {name}
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                  {email}
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  {phone}
-                </Typography>
-              </Grid>
-              <Grid item>
-                <button
-                  className="btn btn-dark btn-sm"
-                  onClick={() => setCurrent(contact)}
-                >
-                  <i className="fas fa-edit"></i> Edit
-                </button>
-                <button className="btn btn-danger btn-sm" onClick={onDelete}>
-                  <i className="fas fa-trash"></i> Delete
-                </button>
-                <Typography variant="body2" style={{ cursor: 'pointer' }}>
-                  Remove
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Typography variant="subtitle1">{type}</Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Paper>
-    </div>
-  );
+	return (
+		<div className={classes.root}>
+			<Paper className={classes.paper}>
+				<Grid container spacing={2}>
+					<Grid item>
+						<ButtonBase className={classes.image}>
+							<img className={classes.img} alt="complex" src={avatar} />
+						</ButtonBase>
+					</Grid>
+					<Grid item xs={12} sm container>
+						<Grid item xs container direction="column" spacing={2}>
+							<Grid item xs>
+								<Typography gutterBottom variant="subtitle1">
+									{name}
+								</Typography>
+								<Typography variant="body2" gutterBottom>
+									{email}
+								</Typography>
+								<Typography variant="body2" color="textSecondary">
+									{phone}
+								</Typography>
+							</Grid>
+							<Grid item>
+								<button
+									className="btn btn-dark btn-sm"
+									onClick={() => setCurrent(contact)}
+								>
+									<i className="fas fa-edit"></i> Edit
+								</button>
+								<button className="btn btn-danger btn-sm" onClick={onDelete}>
+									<i className="fas fa-trash"></i> Delete
+								</button>
+							</Grid>
+						</Grid>
+						<Grid item>
+							<Typography variant="subtitle1">{type}</Typography>
+						</Grid>
+					</Grid>
+				</Grid>
+			</Paper>
+		</div>
+	);
 };
 
 ContactItem.propTypes = {
-  contact: PropTypes.object.isRequired,
+	contact: PropTypes.object.isRequired,
 };
 
 export default ContactItem;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,75 +6,75 @@ import ContactContext from '../../context/contact/ContactContext';
 import styled from 'styled-components';
 
 const StyledImage = styled.img`
-  width: 50px;
-  border-radius: 50%;
+	width: 50px;
+	border-radius: 50%;
+	alignitems: center;
+`;
+
+const StyledLi = styled.li`
+	display: flex;
+	alignitems: center;
+	margin-left: 1px;
 `;
 
 const Navbar = ({ icon, title }) => {
-  const authContext = useContext(AuthContext);
-  const contactContext = useContext(ContactContext);
+	const authContext = useContext(AuthContext);
+	const contactContext = useContext(ContactContext);
 
-  const { isAuthenticated, logout, user } = authContext;
-  const { clearContacts } = contactContext;
+	const { isAuthenticated, logout, user } = authContext;
+	const { clearContacts } = contactContext;
 
-  const onLogout = () => {
-    logout();
-    clearContacts();
-  };
+	const onLogout = () => {
+		logout();
+		clearContacts();
+	};
 
-  const authLinks = (
-    <Fragment>
-      <li>
-        Hello{' '}
-        <StyledImage
-          className="userProfile"
-          src={user && user.avatar}
-          alt="image of user"
-        />
-      </li>
-      <li>
-        <a onClick={onLogout} href="#!">
-          <i className="fas fa-sign-out-alt"></i>
-          <span className="hide-sm">logout</span>
-        </a>
-      </li>
-    </Fragment>
-  );
+	const authLinks = (
+		<Fragment>
+			<StyledLi>
+				<StyledImage
+					className="userProfile"
+					src={user && user.avatar}
+					alt="image of user"
+				/>
+			</StyledLi>
+			<StyledLi>
+				<a onClick={onLogout} href="#!">
+					<i className="fas fa-sign-out-alt"></i>
+					<span className="hide-sm">logout</span>
+				</a>
+			</StyledLi>
+		</Fragment>
+	);
 
-  const guestLinks = (
-    <Fragment>
-      {/* <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li> */}
-      <li>
-        <Link to="/register">Register</Link>
-        <Link to="/login">Login</Link>
-      </li>
-    </Fragment>
-  );
+	const guestLinks = (
+		<Fragment>
+			<li>
+				<Link to="/register">Register</Link>
+				<Link to="/login">Login</Link>
+			</li>
+		</Fragment>
+	);
 
-  return (
-    <div className="navbar bg-primary">
-      <h1>
-        <i className={icon} />
-        {` ${title}`}
-      </h1>
-      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
-    </div>
-  );
+	return (
+		<div className="navbar bg-primary">
+			<h1>
+				<i className={icon} />
+				{` ${title}`}
+			</h1>
+			<ul>{isAuthenticated ? authLinks : guestLinks}</ul>
+		</div>
+	);
 };
 
 Navbar.propTypes = {
-  title: PropTypes.string.isRequired,
-  icon: PropTypes.string,
+	title: PropTypes.string.isRequired,
+	icon: PropTypes.string,
 };
 
 Navbar.defaultProps = {
-  title: 'Contact Keeper',
-  icon: 'fas fa-id-card-alt',
+	title: 'Contact Keeper',
+	icon: 'fas fa-id-card-alt',
 };
 
 export default Navbar;
